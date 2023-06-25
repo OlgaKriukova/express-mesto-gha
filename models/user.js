e97fe6e3780f34c0207bbfa8ad6e1f97b2a4a6e1
Olga Kriukova <lyalyolik.k@yandex.ru>
2023-06-25 21:06:46 +0300

    00001_um добавила валидацию

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const isEmail = require('validator/lib/isEmail');
 
 const userSchema = new mongoose.Schema(
   {
@@ -21,6 +22,11 @@ const userSchema = new mongoose.Schema(
     email: {
       type: String,
       required: true,
+      unique: true,
+      validate: {
+        validator: (v) => isEmail(v),
+        message: 'Неправильный формат почты',
+      },
     },
     password: {
       type: String,
